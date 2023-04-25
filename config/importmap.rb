cf7afd23d9fed250145db8c2b74d6c39303e2411
MiBBy-4 <maksim.762373maks@bk.ru>
2023-04-25 17:30:47 +0300

    feature: implement authentication with devise, also install and configure stimulus and turbo

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,12 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
+pin "postcss", to: "https://ga.jspm.io/npm:postcss@8.4.21/lib/postcss.mjs"
+pin "buffer", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/buffer.js"
+pin "fs", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/fs.js"
+pin "nanoid/non-secure", to: "https://ga.jspm.io/npm:nanoid@3.3.6/non-secure/index.js"
+pin "path", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/path.js"
+pin "picocolors", to: "https://ga.jspm.io/npm:picocolors@1.0.0/picocolors.browser.js"
+pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/process-production.js"
+pin "source-map-js", to: "https://ga.jspm.io/npm:source-map-js@1.0.2/source-map.js"
+pin "url", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/url.js"
